dc52586f5a0ca5486bd4e15b8f8c5e9be21fc783
Anth Richardson <anthrblahblah@gmail.com>
2025-08-05 23:54:08 +0100

    Fixed timescale snapping

--- Assets/Scripts/NetworkTime.cs
+++ Assets/Scripts/NetworkTime.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class NetworkTime : MonoBehaviour
 {
     public float timeScale = 1f;
-    public float adjustmentRate = 0.0025f;
+    public float adjustmentRate = 0.001f;
+    public float targetTimeScale = 1f;
 
     [UsedImplicitly]
     public void OnPlayerUpdated(Player newPlayer)
@@ -13,8 +14,8 @@ public class NetworkTime : MonoBehaviour
         timeScale = newPlayer.SimulationOffset switch
         {
             < 2 => Mathf.MoveTowards(timeScale, 1.2f, adjustmentRate),
-            > 2 => Mathf.MoveTowards(timeScale, 0.8f, adjustmentRate),
-            2 => 1f
+            > 3 => Mathf.MoveTowards(timeScale, 0.8f, adjustmentRate),
+            _ => Mathf.MoveTowards(timeScale, targetTimeScale, adjustmentRate),
         };
 
         Time.timeScale = timeScale;
